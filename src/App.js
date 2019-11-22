d572760785ec7f9c2771483481ddce90628bcae4
Nasyarobby <nasyarobby@gmail.com>
2019-11-22 11:20:05 +0700

    wrapped app with networkchangehandler

--- src/App.js
+++ src/App.js
@@ -8,27 +8,32 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomeRoute from "./Routes/Home";
 import CreateBilling from "./Routes/CreateBilling";
 import Login from "./Routes/Login";
+import { SnackbarProvider } from "notistack";
+import NetworkChangeHandler from "./Components/NetworkChangeHandler";
 
 export default function SimpleContainer() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Router>
-        <ApplicationBar></ApplicationBar>
-        <Container maxWidth="sm" mt={10} style={{ marginTop: 30 }}>
-          <Switch>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/rekam-ssp">
-              <CreateBilling />
-            </Route>
-            <Route path="/">
-              <HomeRoute />
-            </Route>
-          </Switch>
-        </Container>
-      </Router>
+      <SnackbarProvider>
+        <Router>
+          <ApplicationBar></ApplicationBar>
+          <Container maxWidth="sm" mt={10} style={{ marginTop: 30 }}>
+            <Switch>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <Route path="/rekam-ssp">
+                <CreateBilling />
+              </Route>
+              <Route path="/">
+                <HomeRoute />
+              </Route>
+            </Switch>
+          </Container>
+        </Router>
+        <NetworkChangeHandler />
+      </SnackbarProvider>
     </React.Fragment>
   );
 }
