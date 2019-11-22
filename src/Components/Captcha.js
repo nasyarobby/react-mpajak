65a75c9988b579c4223e4810617c9b075b58e6c6
Nasyarobby <nasyarobby@gmail.com>
2019-11-22 14:30:52 +0700

    moved out captcha component

---
+++ src/Components/Captcha.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { withSnackbar } from "notistack";
+
+class Captcha extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { key: "" };
+    this.getSession = this.getSession.bind(this);
+  }
+
+  componentDidMount() {
+    this.getSession();
+  }
+
+  getSession() {
+    axios
+      .put("http://localhost:3000/api/sse/sessions/")
+      .then(resp => {
+        this.setState({ key: resp.data.data.key });
+        localStorage.setItem("key", resp.data.data.key);
+      })
+      .catch(error => {
+        this.props.enqueueSnackbar("Server is offline", { variant: "error" });
+      });
+  }
+
+  render() {
+    const keyExists = !!this.state.key;
+    return (
+      keyExists && (
+        <img
+          alt="captcha"
+          src={
+            "http://localhost:3000/api/sse/sessions/captcha/" + this.state.key
+          }
+        ></img>
+      )
+    );
+  }
+}
+
+export default withSnackbar(Captcha);
