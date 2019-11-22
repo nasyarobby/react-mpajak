61e806105c81eb99f75b9fbdae2c411f93f7464b
Nasyarobby <nasyarobby@gmail.com>
2019-11-22 14:30:14 +0700

    refactored

--- src/Routes/Login.js
+++ src/Routes/Login.js
@@ -1,97 +1,86 @@
-import React from "react";
+import React, { component } from "react";
 import { TextField, Box, Button } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import FormControl from "@material-ui/core/FormControl";
-import InputMask from "react-input-mask";
+import NpwpTextField from "../Components/NpwpTextField";
+import Captcha from "../Components/Captcha";
+
+function formatNpwp(text) {
+  return `${text.slice(0, 2)}.${text.slice(2, 5)}.${text.slice(
+    5,
+    8
+  )}.${text.slice(8, 9)}-${text.slice(9, 12)}.${text.slice(12, 15)}`;
+}
 
 export default function Login(props) {
+  const [npwp, saveNpwp] = React.useState("");
   const [state, saveState] = React.useState({
     npwp: "",
     pin: ""
   });
 
   const ls = window.localStorage;
-  let npwp = ls.getItem("npwp");
+  let npwpLocalStorage = ls.getItem("npwp");
 
   const npwpOnChangeHandler = e => {
-    saveState({ ...state, npwp: e.target.value });
+    saveNpwp(e.target.value);
   };
 
-  function formatNpwp(text) {
-    return `${text.slice(0, 2)}.${text.slice(2, 5)}.${text.slice(
-      5,
-      8
-    )}.${text.slice(8, 9)}-${text.slice(9, 12)}.${text.slice(12, 15)}`;
-  }
-
-  let NpwpComponent = npwp ? (
+  let NpwpComponent = npwpLocalStorage ? (
     <Box fontWeight="fontWeightBold" fontSize="h6.fontSize" textAlign="center">
       {formatNpwp(npwp)}
     </Box>
   ) : (
     <FormControl fullWidth>
-      <InputMask
-        mask="99.999.999.9-999.999"
-        value={state.npwp}
-        onChange={npwpOnChangeHandler}
-        maskChar=""
-      >
-        {inputProps => (
-          <TextField
-            {...inputProps}
-            label="NPWP"
-            variant="outlined"
-          ></TextField>
-        )}
-      </InputMask>
+      <NpwpTextField onChange={npwpOnChangeHandler} value={npwp} />
     </FormControl>
   );
+
+  const wrapInsideGridAndBox = function(component) {
+    return (
+      <Grid item xs={12}>
+        <Box px={5} my={1}>
+          {component}
+        </Box>
+      </Grid>
+    );
+  };
 
   return (
     <div>
       <Grid container direction="row" justify="center" alignItems="center">
-        <Grid item xs={12}>
-          <Box px={5} my={1}>
-            {NpwpComponent}
-          </Box>
-        </Grid>
-        <Grid item xs={12}>
-          <Box px={5} my={1}>
-            <FormControl fullWidth>
-              <TextField
-                type="password"
-                label="PIN"
-                variant="outlined"
-              ></TextField>{" "}
-            </FormControl>
-          </Box>
-        </Grid>
-        <Grid item xs={12}>
-          <Box px={5} my={1}>
-            <FormControl fullWidth>
-              <TextField
-                type="password"
-                label="Captcha"
-                variant="outlined"
-              ></TextField>{" "}
-            </FormControl>
-          </Box>
-        </Grid>
+        {wrapInsideGridAndBox(NpwpComponent)}
+
+        {wrapInsideGridAndBox(
+          <FormControl fullWidth>
+            <TextField
+              type="password"
+              label="PIN"
+              variant="outlined"
+            ></TextField>{" "}
+          </FormControl>
+        )}
+
+        {wrapInsideGridAndBox(
+          <FormControl fullWidth>
+            <TextField
+              type="password"
+              label="Captcha"
+              variant="outlined"
+            ></TextField>{" "}
+          </FormControl>
+        )}
+
         <Grid item xs={12}>
           <Box textAlign="center">
-            <img
-              alt="captcha"
-              src="https://sse3.pajak.go.id/captcha-image"
-            ></img>
+            <Captcha />
           </Box>
         </Grid>
-        <Grid item xs={12}>
-          <Box px={5} my={1}>
-            <Button variant="contained" color="primary" fullWidth>
-              Login
-            </Button>
-          </Box>
-        </Grid>
+        {wrapInsideGridAndBox(
+          <Button variant="contained" color="primary" fullWidth>
+            Login
+          </Button>
+        )}
       </Grid>
     </div>
   );
